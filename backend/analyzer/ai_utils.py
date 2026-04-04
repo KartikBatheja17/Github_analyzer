4599d4cb4389010b722b305419a992a80d0654a2
KartikBatheja17 <bathejasam.17@gmail.com>
2026-04-05 02:44:24 +0530

    fix gemini sdk

--- backend/analyzer/ai_utils.py
+++ backend/analyzer/ai_utils.py
@@ -1,14 +1,13 @@
 import os
 import google.generativeai as genai
 
+# Configure Gemini API
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
 def generate_ai_analysis(profile_data, repos_data):
     try:
-        api_key = os.getenv("GEMINI_API_KEY")
-
-        client = genai.Client(
-            api_key=api_key
-        )
-
         username = profile_data.get("login", "developer")
         repo_count = profile_data.get("public_repos", 0)
         followers = profile_data.get("followers", 0)
@@ -16,29 +15,27 @@ def generate_ai_analysis(profile_data, repos_data):
         prompt = f"""
         Analyze this GitHub developer profile.
 
-        Return the response in plain text format
-        Do NOT use 
-        - markdown
-        - bullet symbols(*)
-
+        Return the response in plain text format.
+        Do NOT use markdown or bullet symbols.
 
         Username: {username}
         Public Repositories: {repo_count}
         Followers: {followers}
 
         Provide:
-        - Skill Level
-        - Strengths
-        - Weaknesses
-        - Career Suggestions
+        Skill Level
+        Strengths
+        Weaknesses
+        Career Suggestions
 
         Keep response short (under 120 words).
         """
 
-        response = client.models.generate_content(
-            model="gemini-2.5-flash-lite",
-            contents=prompt
+        model = genai.GenerativeModel(
+            "gemini-2.5-flash-lite"
         )
+
+        response = model.generate_content(prompt)
 
         return response.text
 
